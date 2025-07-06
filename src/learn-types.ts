5d3854e9d98b05ca8c0fa3f296ac0dbf22c902b9
aki-kun-desu <aki.kun.desu.123@gmail.com>
2025-07-07 07:30:33 +0900

    as constについて学習

---
+++ src/learn-types.ts
@@ -0,0 +1,21 @@
+// as const を学習
+// 正しい使い方
+const foo = {
+    id: 1,
+    name: 'foo',
+} as const;
+// readonlyなので再代入不可能
+foo.id = 2;
+
+export type Hoge = {
+    id: number;
+    name: string;
+};
+
+// 型を指定していると、そっちが優先されてしまう
+const hoge: Hoge = {
+    id: 1,
+    name: 'a',
+} as const;
+
+hoge.id = 2;
